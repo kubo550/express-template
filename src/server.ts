66087f6324148cbed60ddf3f027c71d4482f3a94
jkurdziel <jakub.kurdziel@talkie.ai>
2022-06-28 22:47:14 +0200

    Add helmet config

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,15 @@ import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import path from "path";
-import type {Request, Response} from "express";
+import type { Request, Response } from "express";
 
 // Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config({path: path.join(path.resolve(), process.env.NODE_ENV === 'test' ? `.env.test` : '.env')});
+dotenv.config({
+  path: path.join(
+    path.resolve(),
+    process.env.NODE_ENV === "test" ? `.env.test` : ".env"
+  ),
+});
 
 // Create Express server
 const app = express();
@@ -19,7 +24,17 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // helmet helps you secure your Express apps by setting various HTTP headers.
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    frameguard: false,
+    hidePoweredBy: false,
+    hsts: false,
+    ieNoOpen: false,
+    noSniff: false,
+    xssFilter: false,
+  })
+);
 
 // CORS middleware to allow cross-origin requests and responses in Express apps.
 app.use(cors());
@@ -29,21 +44,21 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 // health-check endpoint
 app.get("/health", (req: Request, res: Response) => {
-    return res.sendStatus(200);
+  return res.sendStatus(200);
 });
 
 // Render index.html page from public folder
 app.get("/home", (req: Request, res: Response) => {
-    return res.sendFile(path.join(__dirname, "../public/index.html"));
+  return res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 //export the server
 export default app;
 
 // don't run the server when running tests
-if (process.env.stage !== 'test') {
-    const port = process.env.port || 3000;
-    app.listen(port, () => {
-        console.log(`Server is listening on port ${port}...`);
-    });
+if (process.env.stage !== "test") {
+  const port = process.env.port || 3000;
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+  });
 }
